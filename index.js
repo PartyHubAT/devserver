e4a83730d92340953c4934dba3aa2a8a027ea63d
ComradeVanti <kingvanti650@gmail.com>
2022-01-05 11:06:22 +0100

    Add logic for starting express server

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 ﻿require('dotenv').config()
+const express = require("express")
+const http = require('http');
 const serverLogic = require(process.env.GAMESERVERLOGICPATH)
+
+const app = express();
+const server = http.createServer(app);
+
+const port = process.env.PORT
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
