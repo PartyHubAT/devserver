c083a8786d7356aff9b8d2f9247332d006943fb1
ComradeVanti <kingvanti650@gmail.com>
2022-01-05 11:11:13 +0100

    Add socket.io

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
 const express = require("express")
 const http = require('http');
 const serverLogic = require(process.env.GAMESERVERLOGICPATH)
+const {Server} = require('socket.io');
+
+// Setup globals
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"],
+    credentials: true,
+    transports: ['websocket', 'polling'],
+  }, allowEIO3: true
+});
+
+// Start server
 
 const port = process.env.PORT
 server.listen(port, () => {
