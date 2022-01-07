5bd113c1082a6fa9979e922c138c4fabf6499d75
ComradeVanti <kingvanti650@gmail.com>
2022-01-07 10:30:58 +0100

    Add better game-server protocol
    Game-server can now emit to specific sockets and gets a list of all players. The  server also only starts once a specific number of players has joined

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ﻿require('dotenv').config()
 const express = require('express')
 const http = require('http')
-const serverLogic = require(process.env.GAMESERVERLOGICPATH)
+const initServerLogic = require(process.env.GAMESERVERLOGICPATH)
 const { Server } = require('socket.io')
 
 // Setup globals
@@ -9,6 +9,7 @@ const { Server } = require('socket.io')
 const app = express()
 const server = http.createServer(app)
 const roomName = process.env.ROOMNAME
+const startPlayerCount = parseInt(process.env.STARTPLAYERCOUNT)
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost:8080',
@@ -18,23 +19,91 @@ const io = new Server(server, {
   },
   allowEIO3: true
 })
-const { register } = serverLogic
+const playerIds = []
 
 // Setup websocket
 
+function playerCount () {
+  return playerIds.length
+}
+
+function addPlayer (socket) {
+  socket.join(roomName)
+  playerIds.push(socket.id)
+  console.log(`Socket ${socket.id} connected. (${playerCount()}/${startPlayerCount})`)
+}
+
+function removePlayer (socket) {
+  socket.leave(roomName)
+  playerIds.splice(playerIds.indexOf(socket.id), 1)
+  console.log(`Socket ${socket.id} disconnect. (${playerCount()}/${startPlayerCount})`)
+}
+
+function startPlayerCountReached () {
+  return playerCount() === startPlayerCount
+}
+
+function isPlayer (socket) {
+  return playerIds.includes(socket.id)
+}
+
+function getSocketById (id) {
+  return io.sockets.sockets.get(id)
+}
+
+function getPlayerSockets () {
+  return playerIds.map(id => getSocketById(id))
+}
+
+function getPlayers () {
+  return playerIds.map(id => ({
+    _id: id
+  }))
+}
+
+function emitToAll (name, data) {
+  console.log(`Server sent "${name}" to all sockets with ${JSON.stringify(data)}`)
+  io.to(roomName).emit(name, data)
+}
+
+function emitToOne (id, eventName, data) {
+  console.log(`Server sent "${eventName}" to "${id}" socket with ${JSON.stringify(data)}`)
+  getSocketById(id).emit(eventName, data)
+}
+
+function startGame () {
+  console.log('Initializing game...')
+
+  const players = getPlayers()
+  const gameServer = initServerLogic(emitToAll, emitToOne, players)
+
+  getPlayerSockets().forEach(socket => {
+    const events = gameServer.events
+    Object.keys(events).forEach(event => {
+      socket.on(event, data => {
+        console.log(`Socket ${socket.id} sent "${event}" to the server with ${JSON.stringify(data)}`)
+        events[event](data)
+      })
+    })
+  })
+
+  console.log('Starting game...')
+
+  gameServer.startGame()
+}
+
 io.on('connection', socket => {
-  function emitRoomMsg (name, data) {
-    console.log(`Socket ${socket.id} sent a message into the room: ${JSON.stringify(data)}`)
-    io.to(roomName).emit(name, data)
+  if (!startPlayerCountReached()) {
+    addPlayer(socket)
+  } else { console.log(`Socket ${socket.id} attempted to join, but the room is already full.`) }
+
+  if (startPlayerCountReached()) {
+    console.log('Start player-count reached.')
+    startGame()
   }
 
-  console.log(`Socket ${socket.id} connected.`)
-  socket.join(roomName)
-  register(socket, emitRoomMsg)
-
   socket.on('disconnect', () => {
-    console.log(`Socket ${socket.id} disconnect.`)
-    socket.leave(roomName)
+    if (isPlayer(socket)) { removePlayer(socket) }
   })
 })
 
@@ -43,4 +112,5 @@ io.on('connection', socket => {
 const port = process.env.PORT
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`)
+  console.log('Waiting for players...')
 })
