24fc1d75d3f6157cd7b886d92fb2cc6f1d7f0175
ComradeVanti <kingvanti650@gmail.com>
2022-01-22 19:20:54 +0100

    Fix restrictive cors
    Cors now allows all origins

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const roomName = process.env.ROOMNAME
 const startPlayerCount = parseInt(process.env.STARTPLAYERCOUNT)
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:8080',
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true,
     transports: ['websocket', 'polling']
